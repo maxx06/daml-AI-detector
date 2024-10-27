caf31e5fd17dc37af35c22105b769b3d49543c25
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-10-26 23:36:34 -0400

    chakra temp

---
+++ website/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import { Provider } from "@/components/ui/provider"
+import { Button }  from "@/components/ui/button"
+import './App.css';
+
+function App() {
+  return (
+    <Provider>
+      <Button/>
+    </Provider>
+  );
+}
+
+export default App;
